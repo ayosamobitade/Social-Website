58813559195bfe2adffa395de538ae5a495ec6bb
UncleAyo <ayosamobitade@gmail.com>
2022-05-29 22:00:04 +0100

    some updates

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import user_login
+import django.contrib.auth impo
 
 
 
 
 urlpatterns = [
     path('login/', user_login, name = 'login'),
+    pat
     
 ]
