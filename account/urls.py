fe302211f43bcb953696b364aca30b5cd870c371
UncleAyo <ayosamobitade@gmail.com>
2022-05-30 08:01:31 +0100

    some updates

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from .views import user_login, dashboard
+from .views import dashboard
 from django.contrib.auth import views as auth_views
 
 
 
 
 urlpatterns = [
-    #path('login/', user_login, name = 'login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-
     path('logout/', auth_views.LogoutView.as_view, name='logout'),
     path("", dashboard, name='dashboard'),
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
