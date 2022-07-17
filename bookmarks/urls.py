115d1be1b63e476db58f5b30c2e89c6aae0992ce
UncleAyo <ayosamobitade@gmail.com>
2022-07-17 20:12:19 +0100

    some updates

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
+    path('register/', views.register, name='register'),
 ]
