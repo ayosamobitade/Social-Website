12506b5e0f45c81ca4d08f649b74cb64db265945
UncleAyo <ayosamobitade@gmail.com>
2022-07-17 20:38:10 +0100

    some updates

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from account.models import Profile
+
 class UserRegistrationForM(forms.ModelForm):
     password = forms.CharField(label='password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
@@ -14,3 +16,14 @@ class UserRegistrationForM(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
