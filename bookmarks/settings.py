aea987edb3bd934e61943d5c66457cfe170a6702
UncleAyo <ayosamobitade@gmail.com>
2022-08-24 22:35:10 +0100

    some update

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -90,8 +90,10 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'socialwebsite',
+        'USER': 'socialwebsite',
+        'PASSWORD' : 'BAbatunde14,.',
     }
 }
 
