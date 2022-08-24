75dd6d2251ab97144dc9db0acef24777e8d08d1f
UncleAyo <ayosamobitade@gmail.com>
2022-08-24 21:41:37 +0100

    some update

--- images/models.py
+++ images/models.py
@@ -7,7 +7,7 @@ class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name = 'images_created', on_delete = models.CASCADE)
     title = models.CharField(max_length = 200)
     slug = models.SlugField(max_length=200, blank = True)
-    url = models.SlugField()
+    url = models.URLField()
     images = models.ImageField(upload_to = 'images/%Y/%m/%d')
     description = models.TextField(blank = True)
     created = models.DateField(auto_now_add = True, db_index = True)
